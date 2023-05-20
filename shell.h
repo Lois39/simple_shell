d9a7bd537245898e6d1e64be63b5717350585ecc
Your Name <you@example.com>
2023-05-19 19:30:05 -0700

    added shell.h file

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+ssize_t my_getline(char **linee, size_t *n, FILE *stream);
+
+#endif
